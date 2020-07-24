b9c91140fa8a6aa488b20f0eac2977b4b45ea1de
VangalapudiSreeja <57428701+VangalapudiSreeja@users.noreply.github.com>
2020-07-25 00:22:37 +0530

    Add files via upload

---
+++ Iterator/src/main/java/ChannelCollection.java
@@ -0,0 +1,12 @@
+
+	public interface ChannelCollection {
+
+		public void addChannel(Channel c);
+		
+		public void removeChannel(Channel c);
+		
+		public ChannelIterator iterator(ChannelTypeEnum type);
+		
+}
+
+
